525575b94abd22e044c7d4f6a13639f820e062dd
Kaiqbal29 <iqbalfaris@students.amikom.ac.id>
2023-03-25 19:24:49 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using oopMobil;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopMobil;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+        mobil mobil1 = new mobil();
+        mobil1.color = "MetaBlack";
+        mobil1.brand = "Nissan GT-R";
+        mobil1.type = "R-36";
+        mobil1.horn = "Ratatata!!";
+        mobil1.doors = 2;
+        mobil1.since = 2027;
+        mobil1.speed = 320;
+
+        mobil1.Gas(mobil1.speed);
+        mobil1.Klakson(mobil1.horn);
+        mobil1.TampilkanInfo();
+
+        Console.ReadKey();
+    }
+}
---
+++ mobil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopMobil
+{
+    internal class mobil
+    {
+        public string color { get; set; }
+        public string brand { get; set; }
+        public string type { get; set; }
+        public string horn { get; set; }
+        public int doors { get; set; }
+        public int since { get; set; }
+        public int speed { get; set; }
+
+        
+        public void Gas(int sound)
+        {
+            Console.WriteLine("Speed");
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1} Km/h.\n", type, speed);
+        }
+        
+        public void Klakson(string sound)
+        {
+            Console.WriteLine("Suara");
+            Console.WriteLine("{0}\n", sound);
+        }
+    
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Method 3:");
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1} model {2} keluaran tahun {3}\ndengan jumlah pintu sebanyak {4}.", color, brand, type, since, doors);
+        }
+    }
+}
+
+
+
